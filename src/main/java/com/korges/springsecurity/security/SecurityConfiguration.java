01e0e1e551d0725a63794f047e87ab7f370b371e
Korges <korgesik@gmail.com>
2020-03-28 19:53:47 +0100

    Enable CSRF validation.

--- src/main/java/com/korges/springsecurity/security/SecurityConfiguration.java
+++ src/main/java/com/korges/springsecurity/security/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.korges.springsecurity.security.UserPermission.USER_READ;
 import static com.korges.springsecurity.security.UserPermission.USER_WRITE;
@@ -24,7 +25,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/api/users/**").hasAuthority(USER_READ.getPermission())
