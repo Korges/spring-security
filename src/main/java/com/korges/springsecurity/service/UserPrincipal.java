177ae2b6bfaaa7449a5422055ab26311adf70c55
Korges <korgesik@gmail.com>
2020-03-29 14:04:55 +0200

    Generate UserPrincipal.

---
+++ src/main/java/com/korges/springsecurity/service/UserPrincipal.java
@@ -0,0 +1,60 @@
+package com.korges.springsecurity.service;
+
+import com.korges.springsecurity.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+class UserPrincipal implements UserDetails {
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+        this.user.getPermissions().forEach(
+                permission -> authorities.add(new SimpleGrantedAuthority(permission.getUserPermission().name()))
+        );
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.user.getRole().name()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
